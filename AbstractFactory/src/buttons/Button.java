aabdb6e9c921b38b9e5e74b3983a685dd111da5a
Hai Mai <maisonhai456@gmail.com>
2026-08-17 09:17:44 +0700

    abstract factory Æpattern

---
+++ AbstractFactory/src/buttons/Button.java
@@ -0,0 +1,10 @@
+package buttons;/*
+* Abstract Factory assumes that you have several families of products,
+* structured into separate class hierarchies (button.Button/Checbox). All products of
+* the same family have the common interface
+* */
+
+
+public interface Button {
+    void paint();
+}
